f4c6a408e21ec22b7c81e130ffe078444d42fd4c
Ethan Brimhall <ethannbrimhall@gmail.com>
2019-02-19 19:32:33 -0500

    update

--- app.js
+++ app.js
@@ -16,36 +16,6 @@ app.get('/', function(req, res) {
 	res.render('index.html');
 });
 
-app.post('*', (req, res) => {
-  var name = req.body.name;
-  var email = req.body.email;
-  var message = req.body.message;
-
-  var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: '',
-      pass: ''
-    }
-  });
-
-  var mailOptionsHost = {
-    to: 'realethanbrimhall@gmail.com',
-    subject: 'Message from: ' + name,
-    text: 'Name: ' + name + '\nEmail: ' + email + '\n\nMessage: ' + message
-  };
-
-  transporter.sendMail(mailOptionsHost, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
-
-	res.redirect(res.req.params[0]);
-
-});
 
 server.listen(process.env.PORT || 3000, () => {
   console.log('Server Started on port 3000');
